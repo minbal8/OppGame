ca8e8f72d614501535143e4f6d9a64530c4afd6b
minbal8 <minbal023@gmail.com>
2020-12-02 13:28:14 +0200

    Fix errors

--- GameClient/Patterns/Builder/SpeedLevelBuilder.cs
+++ GameClient/Patterns/Builder/SpeedLevelBuilder.cs
@@ -4,56 +4,64 @@ using System.IO;
 
 namespace GameClient
 {
-    class SpeedLevelBuilder : LevelBuilder
-    {
-        public SpeedLevelBuilder(Level level) : base(level) { }
+	class SpeedLevelBuilder : LevelBuilder
+	{
+		public SpeedLevelBuilder(Level level) : base(level) { }
 
-        public override void BuildInnerWalls()
-        {
-            string[] lines = File.ReadAllLines("Levels/SpeedLevel/SpeedLevel.txt");
-            foreach (var item in lines)
-            {
-                string[] coords = item.Split(',', ' ');
-                var x1 = int.Parse(coords[0]);
-                var y1 = int.Parse(coords[1]);
-                var x2 = int.Parse(coords[2]);
-                var y2 = int.Parse(coords[3]);
-                _level.AddPart(new Wall(new Point(x1, y1), new Point(x2, y2)));
-            }
-        }
+		public override Level BuildInnerWalls()
+		{
+			string[] lines = File.ReadAllLines("Levels/SpeedLevel/SpeedLevel.txt");
+			foreach (var item in lines)
+			{
+				string[] coords = item.Split(',', ' ');
+				var x1 = int.Parse(coords[0]);
+				var y1 = int.Parse(coords[1]);
+				var x2 = int.Parse(coords[2]);
+				var y2 = int.Parse(coords[3]);
+				_level.AddPart(new Wall(new Point(x1, y1), new Point(x2, y2)));
+			}
 
-        public override void BuildButtonsAndValves()
-        {
-            Console.WriteLine("Building buttons and valves ... ");
-        }
+			return _level;
+		}
 
-        public override void BuildStartAndFinishAreas()
-        {
-            Console.WriteLine("Building start and finish areas ... ");
-        }
+		public override Level BuildButtonsAndValves()
+		{
+			Console.WriteLine("Building buttons and valves ... ");
 
-        public override void BuildTraps()
-        {
-            Console.WriteLine("Building traps ... ");
+			return _level;
+		}
 
-            Factory trapFactory = new TrapFactory();
+		public override Level BuildStartAndFinishAreas()
+		{
+			Console.WriteLine("Building start and finish areas ... ");
 
-            string[] lines = File.ReadAllLines("Levels/SpeedLevel/Traps.txt");
-            foreach (var item in lines)
-            {
-                string[] coords = item.Split(',');
-                var type = int.Parse(coords[0]);
-                var x1 = int.Parse(coords[1]);
-                var y1 = int.Parse(coords[2]);
-                var x2 = int.Parse(coords[3]);
-                var y2 = int.Parse(coords[4]);
+			return _level;
+		}
 
-                Trap temp = trapFactory.CreateTrap(type,
-                    new Point(x1, y1),
-                    new Point(x2, y2));
+		public override Level BuildTraps()
+		{
+			Console.WriteLine("Building traps ... ");
 
-                _level.AddPart(temp);
-            }
-        }
-    }
+			Factory trapFactory = new TrapFactory();
+
+			string[] lines = File.ReadAllLines("Levels/SpeedLevel/Traps.txt");
+			foreach (var item in lines)
+			{
+				string[] coords = item.Split(',');
+				var type = int.Parse(coords[0]);
+				var x1 = int.Parse(coords[1]);
+				var y1 = int.Parse(coords[2]);
+				var x2 = int.Parse(coords[3]);
+				var y2 = int.Parse(coords[4]);
+
+				Trap temp = trapFactory.CreateTrap(type,
+					new Point(x1, y1),
+					new Point(x2, y2));
+
+				_level.AddPart(temp);
+			}
+
+			return _level;
+		}
+	}
 }
